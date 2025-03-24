62c144eb6b99f8c15ec210fabe7d9b0e9938a51e
Wasim Adil <waseemadilcode@gmail.com>
2025-03-24 14:00:43 +0500

    frontend Setup with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-green-500">Blog Application</h1>
+    </>
+  );
+}
+
+export default App;
